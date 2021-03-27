f921b9c5cd8fb8ec61198f042f15e0c32a412d30
tcy1999 <tcy9902@outlook.com>
2021-03-27 17:18:20 +0000

    Update to Ad List #2

--- mysite/ads/urls.py
+++ mysite/ads/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 app_name='ads'
 urlpatterns = [
-    path('', views.AdListView.as_view()),
-    path('ads', views.AdListView.as_view(), name='all'),
+    path('', views.AdListView.as_view(), name='all'),
     path('ad/<int:pk>', views.AdDetailView.as_view(), name='ad_detail'),
     path('ad/create',
         views.AdCreateView.as_view(success_url=reverse_lazy('ads:all')), name='ad_create'),
@@ -12,4 +11,9 @@ urlpatterns = [
         views.AdUpdateView.as_view(success_url=reverse_lazy('ads:all')), name='ad_update'),
     path('ad/<int:pk>/delete',
         views.AdDeleteView.as_view(success_url=reverse_lazy('ads:all')), name='ad_delete'),
+    path('ad_picture/<int:pk>', views.stream_file, name='ad_picture'),
+    path('ad/<int:pk>/comment',
+        views.CommentCreateView.as_view(), name='ad_comment_create'),
+    path('comment/<int:pk>/delete',
+        views.CommentDeleteView.as_view(success_url=reverse_lazy('ads')), name='ad_comment_delete'),
 ]
